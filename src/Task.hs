49a7e62c7fe1fb4f237d8963036b61077c4a5042
Рауф Ибишов <romf99@yandex.ru>
2020-04-20 22:06:52 +0300

    Third Task

--- src/Task.hs
+++ src/Task.hs
@@ -9,21 +9,33 @@ window :: Display
 window = InWindow "MyWindow" (700, 600) (250, 30)
 
 background :: Color
-background = mixColors 0.9 0.3 red green
+background = mixColors 0.2 0.6 red blue
 
---drawing :: Picture
---drawing = circle 80
+myCircle :: Picture
+myCircle = circle 80
 
-baseWin :: Display
-baseWin = window
+myRectangle :: Picture
+myRectangle = lineLoop (rectanglePath 250 250)
 
-renderWin :: Display -> Picture
-renderWin _ = Blank
+--mH :: Path
+--mH = [(20, 0), (10, 20), (-10, 20), (-20, 0), (-10, -20), (10, -20)]
 
-handleWin :: Event -> Display -> Display
+myHex :: Picture
+myHex = lineLoop [(50, 0), (30, 50), (-30, 50), (-50, 0), (-30, -50), (30, -50)]
+
+myText :: Picture
+myText = translate (-330) 150 (text "Extra thick")
+
+baseWin :: Picture
+baseWin = pictures [myCircle, myRectangle, myText, myHex]
+
+renderWin :: Picture -> Picture
+renderWin x = x
+
+handleWin :: Event -> Picture -> Picture
 handleWin _ x = x
 
-updateWin :: Float -> Display -> Display
+updateWin :: Float -> Picture -> Picture
 updateWin _ x = x
 
 run :: IO ()
